972a8ec5824f00b3378ae0aac44495c535b93e8d
kamii1112 <kamiidaisuke@gmail.com>
2025-03-10 20:42:18 +0530

    added swagger

--- backend/settings.py
+++ backend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API Title",  # Change to your API's title
+      default_version='v1',
+      description="Your API description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourdomain.com"),  # Your email here
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('database/', include("database.urls")),
     path('table/',include("table.urls")),
